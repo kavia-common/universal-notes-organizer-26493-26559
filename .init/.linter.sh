4146c987228c1192e8b91f7902e0ab91886c7408
Kavia AI <production@kavia.ai>
2025-09-26 05:26:47 +0000

    CheckPoint - cgf44f8df0

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/universal-notes-organizer-26493-26559/notes_app_frontend
+flutter analyze
+
---
+++ notes_app_frontend/android/app/src/main/kotlin/com/example/notes_app_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.notes_app_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
